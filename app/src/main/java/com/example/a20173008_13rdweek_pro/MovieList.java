96857dc8c2554fddad136562f6226d9a5eeebea5
illineyeo <aeyongcat@naver.com>
2021-12-03 18:23:58 +0900

    13주차 과제 최종

---
+++ app/src/main/java/com/example/a20173008_13rdweek_pro/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.a20173008_13rdweek_pro;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/a20173008_13rdweek_pro/MovieListResult.java
@@ -0,0 +1,11 @@
+package com.example.a20173008_13rdweek_pro;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList=new ArrayList<Movie>();
+}
